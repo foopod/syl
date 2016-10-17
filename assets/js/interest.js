e3fa4b9031db17a43c39e9b75138ac48ce9f8a62
Jonathon Shields <jonathonshields@gmail.com>
2016-10-18 10:01:09 +1300

    fixed redirect

--- assets/js/interest.js
+++ assets/js/interest.js
@@ -12,7 +12,7 @@ function GET(variable)
 
 function init(){
     var title = decodeURI(GET('title'));
-    var found = false;
+    
     if(title.length > 2){
          $.jGFeed('http://backend.deviantart.com/rss.xml?q=gallery%3AAstral-Haze%2F127863&type=deviation',
               function(feeds){
@@ -22,6 +22,7 @@ function init(){
                   return false;
                 }
                 // do whatever you want with feeds here
+                var found = false;
                 for(var i=0; i<feeds.entries.length; i++){
                   var entry = feeds.entries[i];
                   // Entry title
@@ -36,9 +37,11 @@ function init(){
                     }
                 }
              if(found != true){
-        window.location.replace("http://astral-haze.site");
-    }
+                window.location.replace("http://astral-haze.site");
+            }
               }, 20);
+    } else {
+     window.location.replace("http://astral-haze.site");   
     }
     
 }
